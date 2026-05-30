9269fa2a182c267b8a364c9186f166680d9d52c8
Manoj <manojveerasrinnivaskolla123450@gmail.com>
2026-05-30 22:06:23 +0530

    springboot basics project

---
+++ springBoot-Basics/src/test/java/com/example/bigProject/BigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
