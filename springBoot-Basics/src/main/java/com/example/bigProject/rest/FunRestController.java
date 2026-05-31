793588b8ac6a54714f604d0c28092f0d5b512695
Manoj <manojveerasrinnivaskolla123450@gmail.com>
2026-05-31 14:20:46 +0530

    completed springboot basics

--- springBoot-Basics/src/main/java/com/example/bigProject/rest/FunRestController.java
+++ springBoot-Basics/src/main/java/com/example/bigProject/rest/FunRestController.java
@@ -1,12 +1,37 @@
 package com.example.bigProject.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+
+     //injecting properties values
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+
+    @GetMapping("/teaminfo")
+    public String team(){
+        return teamName;
+    }
+
+    @GetMapping("/coachinfo")
+    public String coach(){
+        return coachName;
+    }
     @GetMapping("/")
     public String Hello(){
          return "Hello world";
+    }
+
+    @GetMapping("/workout")
+    public String workout(){
+        return "Hello world workoutting 5k";
     }
 }
