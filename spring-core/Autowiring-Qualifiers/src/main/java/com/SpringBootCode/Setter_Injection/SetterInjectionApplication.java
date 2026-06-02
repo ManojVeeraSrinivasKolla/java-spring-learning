ff7c4246347207c0d91a49db57df582178239950
Manoj <manojveerasrinnivaskolla123450@gmail.com>
2026-06-02 23:37:45 +0530

    added  spring core - Auto-wired & Qualifiers.  project

---
+++ spring-core/Autowiring-Qualifiers/src/main/java/com/SpringBootCode/Setter_Injection/SetterInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootCode.Setter_Injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetterInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetterInjectionApplication.class, args);
+	}
+
+}
---
+++ spring-core/Autowiring-Qualifiers/src/test/java/com/SpringBootCode/Setter_Injection/SetterInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootCode.Setter_Injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetterInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
