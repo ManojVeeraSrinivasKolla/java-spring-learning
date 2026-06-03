efeb741aa88d2e277b5c0c56f912c0a76e187da9
Manoj <manojveerasrinnivaskolla123450@gmail.com>
2026-06-03 21:55:09 +0530

    completed  spring core - singelton & prototype scopes along bean lifecycle methods

--- spring-core/Bean-Scopes/Spring-demo/src/main/java/com/SpringBootCode/Spring_demo/DemoController.java
+++ spring-core/Bean-Scopes/Spring-demo/src/main/java/com/SpringBootCode/Spring_demo/DemoController.java
@@ -11,6 +11,7 @@ public class DemoController {
 
     //define private field for the dependency injection
     private Coach myCoach;
+//    private Coach anotherCoach;
 
 //
 //    //dependency injection
@@ -28,10 +29,19 @@ public class DemoController {
 //        myCoach=thecoach;
 //    }
 
+//    @Autowired
+//    public DemoController(@Qualifier("cricketCoach") Coach thecoach,
+//                          @Qualifier("cricketCoach") Coach othercoach){
+//        System.out.println("In constructor : "+getClass().getSimpleName());
+//        myCoach=thecoach;
+//        anotherCoach=othercoach;
+//    }
+
     @Autowired
-    public void  setCoach (Coach thecoach){
+    public DemoController(@Qualifier("cricketCoach") Coach thecoach){
         System.out.println("In constructor : "+getClass().getSimpleName());
         myCoach=thecoach;
+
     }
 
 
@@ -40,6 +50,12 @@ public class DemoController {
     @GetMapping("/dailyWorkOut")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkOut();
+
     }
+
+//    @GetMapping("/check")
+//    public String check(){
+//        return "comparing beans: myCoach=anotherCoach, "+(myCoach==anotherCoach);
+//    }
 
 }
