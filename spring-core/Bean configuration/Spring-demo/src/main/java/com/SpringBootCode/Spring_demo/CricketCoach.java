2b67bddf0d036cb267719d37a782a112c6bf3788
Manoj <manojveerasrinnivaskolla123450@gmail.com>
2026-06-03 22:12:38 +0530

    added  spring core - java configuration for beans

---
+++ spring-core/Bean configuration/Spring-demo/src/main/java/com/SpringBootCode/Spring_demo/CricketCoach.java
@@ -0,0 +1,33 @@
+package com.SpringBootCode.Spring_demo;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class CricketCoach implements Coach {
+
+    public CricketCoach(){
+        System.out.println("In constructor : "+getClass().getSimpleName());
+    }
+
+    // define init method
+
+    @PostConstruct
+    public void myinit(){
+        System.out.println(" In domystartupstuff() :"+getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void cleanstuff(){
+        System.out.println(" In domycleanuppstuff() :"+getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkOut() {
+        return "practice slot bowiling for 55 minutes -!!!!!";
+    }
+}
---
+++ spring-core/Bean configuration/Spring-demo/src/main/java/com/SpringBootCode/Spring_demo/DemoController.java
@@ -0,0 +1,61 @@
+package com.SpringBootCode.Spring_demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define private field for the dependency injection
+    private Coach myCoach;
+//    private Coach anotherCoach;
+
+//
+//    //dependency injection
+//    @Autowired
+//    private Coach myCoach;
+//    //defining the constructor for dependency injection
+
+//    @Autowired
+//    public DemoController (@Qualifier("tennisCoach") Coach thecoach){
+//         myCoach=thecoach;
+//    }
+
+//    @Autowired
+//    public void  setCoach (@Qualifier("cricketCoach") Coach thecoach){
+//        myCoach=thecoach;
+//    }
+
+//    @Autowired
+//    public DemoController(@Qualifier("cricketCoach") Coach thecoach,
+//                          @Qualifier("cricketCoach") Coach othercoach){
+//        System.out.println("In constructor : "+getClass().getSimpleName());
+//        myCoach=thecoach;
+//        anotherCoach=othercoach;
+//    }
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach thecoach){
+        System.out.println("In constructor : "+getClass().getSimpleName());
+        myCoach=thecoach;
+
+    }
+
+
+
+
+    @GetMapping("/dailyWorkOut")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkOut();
+
+    }
+
+//    @GetMapping("/check")
+//    public String check(){
+//        return "comparing beans: myCoach=anotherCoach, "+(myCoach==anotherCoach);
+//    }
+
+}
