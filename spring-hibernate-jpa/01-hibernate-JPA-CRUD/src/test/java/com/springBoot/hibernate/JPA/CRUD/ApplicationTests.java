e6c7a54d8d3e83db305e9e40eac610b910a27f67
Manoj <srinivaskolla.scet@gmail.com>
2026-06-11 11:32:49 +0530

    added  spring Hibernate/Data JPA- CommandLineRunner

---
+++ spring-hibernate-jpa/01-hibernate-JPA-CRUD/src/test/java/com/springBoot/hibernate/JPA/CRUD/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.hibernate.JPA.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
